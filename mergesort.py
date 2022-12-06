9048fbb4ae9c3136103a56055314ff6516fa5c2d
Nagham hesham <119948289+Nag-ham@users.noreply.github.com>
2022-12-06 16:28:12 +0200

    Add files via upload

---
+++ mergesort.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def mergeSort(arr):
+     if len(arr) > 1:
+
+        mid = len(arr)//2
+
+        L = arr[:mid]
+        
+        R = arr[mid:]
+        
+        mergeSort(L)
+        
+        mergeSort(R)
+        
+        i = j = k = 0
+
+
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+                k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+    
+if __name__ == '__main__':
+    arr = [41, 54, 26, 41, 36, 16]
+    print("before sorting", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
+
+
+
+# In[ ]:
+
+
+
+
